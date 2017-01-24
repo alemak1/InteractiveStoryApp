b20fa006396cb261dbc02807306181787abeed83
yongrenwang <czie.alexmak@gmail.com>
2017-01-24 19:23:32 +0800

    Subclassed UIViewController to create PageController and set this as the custom viewcontroller of the UIViewController to which the launch page links

---
+++ InteractiveStory/InteractiveStory/PageController.swift
@@ -0,0 +1,21 @@
+//
+//  PageController.swift
+//  InteractiveStory
+//
+//  Created by Aleksander Makedonski on 1/24/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PageController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
