8e3e33d7c9793a1a4aff2659d9e2369fd7955ee5
yongrenwang <czie.alexmak@gmail.com>
2017-01-24 18:38:52 +0800

    Added the Page model to our Story file

--- InteractiveStory/InteractiveStory/Story.swift
+++ InteractiveStory/InteractiveStory/Story.swift
@@ -53,3 +53,18 @@ extension Story{
         }
     }
 }
+
+
+class Page{
+    let story: Story
+    
+    typealias Choice = (title: String, page: Page)
+    
+    var firstChoice: Choice?
+    var secondChoice: Choice?
+    
+    init(story: Story){
+        self.story = story
+    }
+    
+}
