5470dd4a91bed5a556dcd7e9825390df7bae6aab
yongrenwang <czie.alexmak@gmail.com>
2017-01-24 18:09:52 +0800

    Added a file for Story enum

---
+++ InteractiveStory/InteractiveStory/Story.swift
@@ -0,0 +1,24 @@
+//
+//  Story.swift
+//  InteractiveStory
+//
+//  Created by Aleksander Makedonski on 1/24/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+enum Story{
+    case ReturnTrip
+    case TouchDown
+    case Homeward
+    case Rover
+    case Cave
+    case Crate
+    case Monster
+    case Droid
+    case Home
+    
+}
